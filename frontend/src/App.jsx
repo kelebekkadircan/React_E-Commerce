994ff39f81e9bf30c2957c64c9dead5adc10b063
kelebekkadircan <kelebekkadircan@gmail.com>
2023-12-10 16:45:34 +0300

    Mainpage_layout_Day1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './app.css'
+import Footer from './components/Layout/Footer/Footer'
+import Header from './components/Layout/Header/Header'
+import Policy from './components/Layout/Policy/Policy'
+import Sliders from './components/Slider/Sliders'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Sliders />
+      <Policy />
+      <Footer />
+    </>
+  )
+}
+
+export default App
